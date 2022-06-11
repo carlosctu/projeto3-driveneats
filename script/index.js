3ea94f6968f1acf25e341d4e93a06f5a188b12c0
carlosctu <carlosctu94@gmail.com>
2022-06-11 14:50:51 -0300

    Ajustes

--- script/index.js
+++ script/index.js
@@ -1,8 +1,16 @@
 let dish;
 let mainDish;
 let drink;
+let chosedDish;
+let chosedMainDish;
+let chosedDrink;
 
 function chosedOption(element) {
+  dish = document.querySelector(".menu .chosed");
+  // console.log(dish);
+  mainDish = document.querySelector(".menu-main .chosed");
+  drink = document.querySelector(".menu-drink .chosed");
+
   dish = document.querySelector(".menu .chosed");
   mainDish = document.querySelector(".menu-main .chosed");
   drink = document.querySelector(".menu-drink .chosed");
@@ -23,18 +31,64 @@ function closeOrder() {
   dish = document.querySelector(".menu .chosed");
   mainDish = document.querySelector(".menu-main .chosed");
   drink = document.querySelector(".menu-drink .chosed");
-  
-  let closeOrder = document.querySelector(".button button")
+
+  let closeOrder = document.querySelector(".button button");
 
   if (dish && mainDish && drink) {
     closeOrder.classList.add("done");
-    closeOrder.innerHTML = "Finalizar Pedido"
-    closeOrder.disabled = false
+    closeOrder.innerHTML = "Finalizar Pedido";
+    closeOrder.disabled = false;
   }
 }
 
 function checkOut() {
-    let nome = prompt("Qual é seu nome?")
-    let endereço = prompt("Qual é seu endereço?")
-    document.querySelector(".main").classList.add("blur")
+  document.querySelector(".main").classList.add("blur");
+  let teste = document.querySelector(".checkOut").classList.remove("hidden")
+  let chosedDish = document.querySelector(
+    ".menu .chosed .chosedDish"
+  ).innerHTML;
+  let chosedDishPrice = Number(
+    document.querySelector(".menu .chosed .value").innerHTML
+  );
+  let chosedMainDish = document.querySelector(
+    ".menu-main .chosed .chosedDish"
+  ).innerHTML;
+  let chosedMainDishPrice = Number(
+    document.querySelector(".menu-main .chosed .value").innerHTML
+  );
+  let chosedDrink = document.querySelector(
+    ".menu-drink .chosed .chosedDish"
+  ).innerHTML;
+  let chosedDrinkPrice = Number(
+    document.querySelector(".menu-drink .chosed .value").innerHTML
+  );
+  let checkOutValue = Number(
+    (chosedDrinkPrice + chosedMainDishPrice + chosedDishPrice).toFixed(2)
+  );
+  document.querySelector(".dishOrder .dishName").innerHTML = chosedDish;
+  document.querySelector(".dishOrder .dishValue").innerHTML =
+    chosedDishPrice.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  document.querySelector(".mainDishOrder .dishName").innerHTML = chosedMainDish;
+  document.querySelector(".mainDishOrder .dishValue").innerHTML =
+    chosedMainDishPrice.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  document.querySelector(".drinkOrder .dishName").innerHTML = chosedDrink;
+  document.querySelector(".drinkOrder .dishValue").innerHTML =
+    chosedDrinkPrice.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  document.querySelector(".checkOut .totalValue").innerHTML =
+    checkOutValue.toLocaleString("pt-br", {
+      style: "currency",
+      currency: "BRL",
+    });
+  // console.log(teste)
+  // let nome = prompt("Qual é seu nome?")
+  // let endereço = prompt("Qual é seu endereço?")
 }
